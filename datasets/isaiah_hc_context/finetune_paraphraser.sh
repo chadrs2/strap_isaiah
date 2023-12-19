02fddb218360f8d0e63e8f9e8928cbc7f527dee3
Chad Samuelson <crsamuelson87@gmail.com>
2023-12-18 18:38:43 -0700

    Updated isaiah hard coded context .sh files

--- datasets/isaiah_hc_context/finetune_paraphraser.sh
+++ datasets/isaiah_hc_context/finetune_paraphraser.sh
@@ -11,14 +11,14 @@
 # Experiment Details :- GPT2-large model for paraphrasing.
 # Run Details :- accumulation = 2, batch_size = 5, beam_size = 1, cpus = 3, dataset = datasets/paranmt_filtered, eval_batch_size = 1, global_dense_feature_list = none, gpu = m40, learning_rate = 5e-5, memory = 50, model_name = gpt2-large, ngpus = 1, num_epochs = 3, optimizer = adam, prefix_input_type = original, save_steps = 500, save_total_limit = -1, specific_style_train = -1, stop_token = eos
 
-export DATA_DIR=datasets/isaiah_hc_context_finetuned_paraphraser #paranmt_filtered
+export DATA_DIR=datasets/isaiah_context_eng_finetuned_paraphraser #paranmt_filtered
 
 # source style-venv/bin/activate
 
 BASE_DIR=style_paraphrase
 
 python3 -m torch.distributed.launch --nproc_per_node=1 $BASE_DIR/run_lm_finetuning.py \
-    --output_dir=$BASE_DIR/saved_models/isaiah_hc_finetuned_paraphraser \
+    --output_dir=$BASE_DIR/saved_models/isaiah_finetuned_paraphraser \
     --model_type=gpt2 \
     --model_name_or_path=paraphraser_gpt2_large \
     --do_train \
@@ -30,7 +30,7 @@ python3 -m torch.distributed.launch --nproc_per_node=1 $BASE_DIR/run_lm_finetuni
     --num_train_epochs 3 \
     --gradient_accumulation_steps 2 \
     --per_gpu_train_batch_size 5 \
-    --job_id isaiah_hc_finetuning_paraphraser \
+    --job_id isaiah_finetuning_paraphraser \
     --learning_rate 5e-5 \
     --prefix_input_type original \
     --global_dense_feature_list none \
